fe1b75e042d1c50f8476f5dd444232731be61b36
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2013-04-27 17:44:15 +0000

    Refactorings, flash error integration

--- app/controllers/edit/PickOutPlacesController.scala
+++ app/controllers/edit/PickOutPlacesController.scala
@@ -1,7 +1,7 @@
 package controllers.edit
 
 import play.api._
-import play.api.mvc.{Controller,Action}
+import play.api.mvc.{Controller,Action,Request,Result}
 import play.api.Play.current
 
 import play.api.data._
@@ -12,6 +12,8 @@ import play.api.db.slick.Config.driver.simple._
 
 import models._
 
+import views.html.edit.pickoutplaces
+
 object PickOutPlacesController extends Controller {
   val pickOutPlaceForm = Form(
     mapping(
@@ -20,53 +22,57 @@ object PickOutPlacesController extends Controller {
     )(NewPickOutPlace.apply)(NewPickOutPlace.unapply)
   )
 
-  def index = Action {
-    DB.withSession { implicit session: Session =>
-      Ok(views.html.edit.pickoutplaces.index(PickOutPlaces.findAll))
+  private def withExisting[A](id: Long)(f: PickOutPlace => Result)(implicit request: Request[A]) = {
+    DB.withTransaction { implicit session =>
+      val place = PickOutPlaces findById id
+      place match {
+        case None => BadRequest(pickoutplaces.index(PickOutPlaces.findAll)).flashing("error" -> ("Место отбора не найдено: " + id.toString))
+        case Some(p) => f(p)
+      }
     }
   }
 
-  def create = Action {
-    Ok(views.html.edit.pickoutplaces.create(pickOutPlaceForm))
+  def index = Action { implicit request =>
+    DB.withTransaction { implicit session =>
+      Ok(pickoutplaces.index(PickOutPlaces.findAll))
+    }
+  }
+
+  def create = Action { implicit request =>
+    Ok(pickoutplaces.create(pickOutPlaceForm))
   }
 
   def save = Action { implicit request =>
     pickOutPlaceForm.bindFromRequest.fold(
       errors => {
-        BadRequest(views.html.edit.pickoutplaces.create(errors))
+        BadRequest(pickoutplaces.create(errors))
       },
       newPlace => {
-        DB.withSession { implicit session: Session =>
+        DB.withTransaction { implicit session =>
           PickOutPlaces add newPlace
         }
         Redirect(routes.PickOutPlacesController.index)
       })
   }
 
-  def edit(id: Long) = Action {
-    DB.withSession { implicit session: Session =>
-      val place = PickOutPlaces.findById(id)
-      place match {
-        case None => BadRequest(views.html.edit.pickoutplaces.index(PickOutPlaces.findAll))
-        case Some(p) => Ok(views.html.edit.pickoutplaces.edit(p, pickOutPlaceForm.fill(p.asNew)))
-      }
+  def edit(id: Long) = Action { implicit request =>
+    withExisting(id) { p =>
+      Ok(pickoutplaces.edit(p, pickOutPlaceForm.fill(p.asNew)))
     }
   }
 
   def update(id:Long) = Action { implicit request =>
-    DB.withSession { implicit session: Session =>
-      pickOutPlaceForm.bindFromRequest.fold(
-        errors => {
-          val place = PickOutPlaces.findById(id)
-          place match {
-            case None => BadRequest(views.html.edit.pickoutplaces.index(PickOutPlaces.findAll))
-            case Some(p) => BadRequest(views.html.edit.pickoutplaces.edit(p, errors))
-          }
-        },
-        newPlaceValue => {
+    pickOutPlaceForm.bindFromRequest.fold(
+      errors => {
+        withExisting(id) { p =>
+          BadRequest(pickoutplaces.edit(p, errors))
+        }
+      },
+      newPlaceValue => {
+        DB.withTransaction { implicit session =>
           PickOutPlaces.update(id, newPlaceValue)
-          Redirect(routes.PickOutPlacesController.index)
-        })
-    }
+        }
+        Redirect(routes.PickOutPlacesController.index)
+      })
   }
 }
--- test/views/edit/pickoutplaces/IndexViewSpec.scala
+++ test/views/edit/pickoutplaces/IndexViewSpec.scala
@@ -3,19 +3,21 @@ import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 
+import play.api.mvc.Flash
+
 import models._
 
 class PickOutPlacesListViewSpec extends Specification {
   "pick-out places list view" should {
     "render titles for all passed in places" in {
       val places = List(PickOutPlace(132, "First test place", "<p>111</p>"), PickOutPlace(15, "Second test place", "222"))
-      val html = views.html.edit.pickoutplaces.index(places)
+      val html = views.html.edit.pickoutplaces.index(places)(new Flash())
       contentAsString(html) must contain("First test place")
       contentAsString(html) must contain("Second test place")
     }
 
     "render message about absent data when no places is passed" in {
-      val html = views.html.edit.pickoutplaces.index(List())
+      val html = views.html.edit.pickoutplaces.index(List())(new Flash())
       contentAsString(html) must contain("Пока не добавлено ни одного места отбора.")
     }
   }
