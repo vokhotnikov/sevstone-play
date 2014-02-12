41f2b73792854bfcde097982816b8f8c36b13983
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2014-02-12 17:35:16 +0200

    Remove top testimonials section

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,15 +6,13 @@ import play.api.db.slick._
 import models._
 import play.api.Play.current
 
-case class HomeViewModel(randomSpecimen: Option[Loaded[SpecimenWithImages]],
-  latestArticles: List[Loaded[Article]], latestTestimonials: List[Loaded[Testimonial]])
+case class HomeViewModel(randomSpecimen: Option[Loaded[SpecimenWithImages]], latestArticles: List[Loaded[Article]])
 
 trait ApplicationController extends Controller { this: ModelServicesComponent =>
   def home = DBAction { implicit rs =>
     val model = HomeViewModel(
       Services.SpecimenService.randomSpecimen,
-      Services.ArticleService.latestArticles(3),
-      Services.TestimonialService.latestTestimonials(3))
+      Services.ArticleService.latestArticles(3))
 
     Ok(views.html.app.home(model))
   }
